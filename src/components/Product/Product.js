6d1a5cc7cc5d9687f59057961b806074fef1b76d
Anna Burnos <burnos.anna@gmail.com>
2024-02-07 09:00:07 +0100

    Add useMemo to getPrice function and change its name to price

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,6 @@
 import styles from './Product.module.scss';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import ProductImage from '../ProductImage/ProductImage';
 import ProductForm from '../ProductForm/ProductForm';
 
@@ -9,17 +9,17 @@ const Product = props => {
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
 
-  const getPrice = () => {
+  const price = useMemo(() => {
     const selectedSize = props.sizes.find((size) => size.name === currentSize);
     return props.basePrice + selectedSize.additionalPrice;
-  };
+  }, [currentSize]);
 
   const handleAddToCart = event => {
     event.preventDefault();
 
     console.log('Summary:');
     console.log('Name: ', props.title);
-    console.log('Price: ', getPrice());
+    console.log('Price: ', price);
     console.log('Size: ', currentSize);
     console.log('Color: ', currentColor);
   };
@@ -30,7 +30,7 @@ const Product = props => {
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <span className={styles.price}>Price: {price}$</span>
         </header>
         <ProductForm action={handleAddToCart} sizes={props.sizes} currentSize={currentSize} currentColor={currentColor} colors={props.colors} setCurrentSize={setCurrentSize} setCurrentColor={setCurrentColor} />
       </div>
